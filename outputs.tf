012272768f59aab0d9f609ae94e82cc2670bbdc6
FabianIMV <fabianignaciomv@gmail.com>
2024-10-21 17:00:07 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,7 @@ output "grafana_url" {
   description = "URL para acceder a Grafana"
   value       = try("http://localhost:${data.kubernetes_service.grafana.spec[0].port[0].node_port}", "Grafana URL not available yet")
 }
+
 data "kubernetes_service" "grafana" {
   metadata {
     name = "grafana"
